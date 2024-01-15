a689258179b11f5cf5420fb652ddeec61acecdb5
arkaardiansyah <142191547+arkaardiansyah@users.noreply.github.com>
2024-01-15 09:43:57 +0700

    Add files via upload

---
+++ pertemuan 1/perkenalan.py
@@ -0,0 +1,7 @@
+nama="arka"
+kelas="tkj xa"
+jurusan="teknik komputer dan jaringan"
+
+
+print("prekenalkan nama saya arka")
+print("saya kelas",tkj xa,"dari jurusan",teknik komputer jaringan)
---
+++ pertemuan 1/variable.py
@@ -0,0 +1,18 @@
+x = 8
+y = 5
+z = "SMK Medikacom"
+p = False
+q = True
+
+print(x)
+print(y)
+print(z)
+print(p)
+print(q)
+
+print("cek tipe data")
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(p))
+print(type(q))
